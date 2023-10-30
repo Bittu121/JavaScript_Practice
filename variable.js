c8f799abceac142bfcd23b85dcc013f8d71e7e76
Bittu Kumar <bk4983105@gmail.com>
2023-10-30 16:24:15 +0530

    initial commit

--- variable.js
+++ variable.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+console.log("hello");
+
+const accountId = 144553;   //value cant't change
+accountId = 2 //not allowed
+console.log(accountId);
+
+
+// let accountEmail = "bittukumar@gmail.com";
+// accountEmail = "hc@hc.com"
+// var accountPassword = "12345";
+// accountPassword = "21212121"
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+// accountCity = "Jaipur";
+// accountCity = "Bengaluru"
+
+// let accountState;   //o/p:- undefined 
+
+// console.table([accountEmail,accountPassword,accountCity,accountState]);
