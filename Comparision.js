8da8474e7fb7827d70ab819c7afc77efdb65172e
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-10-30 16:40:14 +0530

    Add files via upload

---
+++ Comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+// console.log(undefined); //undefined
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+// === strict Check dataype and value both are same then true otherwise give false
+//console.log("2" === 2); //false
+
+// please write (: Clean Code for production purpose because use in Company Adobe)
