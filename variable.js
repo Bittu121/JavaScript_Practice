47b68917064a0853f2f450e7d7bd6ee9be6692bc
Bittu Kumar <bk4983105@gmail.com>
2023-10-30 16:27:19 +0530

    initial commit

--- variable.js
+++ variable.js
@@ -1,14 +1,16 @@
 console.log("hello");
 
-const accountId = 144553;   //value cant't change
-accountId = 2 //not allowed
-console.log(accountId);
+// const accountId = 144553;   //value cant't change
+// accountId = 2 //not allowed
+// console.log(accountId);
 
 
-// let accountEmail = "bittukumar@gmail.com";
-// accountEmail = "hc@hc.com"
-// var accountPassword = "12345";
-// accountPassword = "21212121"
+let accountEmail = "bittukumar@gmail.com";
+accountEmail = "hc@hc.com"
+console.log(accountEmail);
+var accountPassword = "12345";
+accountPassword = "21212121"
+console.log(accountPassword);
 /*
 prefer not use var
 because of issue in block scope and functional scope
