ca2b4c1fa2b1c19699daf28b848542724d827a92
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-10-30 16:41:17 +0530

    Update Comparision.js

--- Comparision.js
+++ Comparision.js
@@ -4,17 +4,17 @@
 // console.log(2<=1);
 // console.log(2!=1);
 
-// console.log("2">1);//true
-// console.log("02">1);//true
+// console.log("2">1); //true
+// console.log("02">1); //true
 
-// console.log(null > 0);//false
-// console.log(null == 0);//false
-// console.log(null >= 0);//true
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
 
 // console.log(undefined); //undefined
-// console.log(undefined == 0);//false
-// console.log(undefined > 0);//false
-// console.log(undefined < 0);//false
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
 
 // === strict Check dataype and value both are same then true otherwise give the result false
 //console.log("2" === 2); //false
