cda3ad993d980774d1bf679e246c7ba5113b8d7e
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-11-03 22:23:42 +0530

    Update nums_math.js

--- nums_math.js
+++ nums_math.js
@@ -1,38 +1,38 @@
 // +++++++++++++Numbers+++++++++++++++++++++++++++++
 const score = 400
-console.log(score);//400
+console.log(score); //400
 
 const balance = new Number(100)
-console.log(balance);//[Number: 100]
+console.log(balance); //[Number: 100]
 console.log(balance.toString().length); //3
-console.log(balance.toFixed(1));//100.0
-console.log(balance.toFixed(2));//100.00
+console.log(balance.toFixed(1)); //100.0
+console.log(balance.toFixed(2)); //100.00
 
 const otherNumber = 123.8966
-console.log(otherNumber.toPrecision(3));//124
-console.log(otherNumber.toPrecision(4));//123.9
-console.log(otherNumber.toPrecision(5));//123.90
+console.log(otherNumber.toPrecision(3)); //124
+console.log(otherNumber.toPrecision(4)); //123.9
+console.log(otherNumber.toPrecision(5)); //123.90
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+console.log(hundreds.toLocaleString('en-IN')); //10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-console.log(Math);//Object [Math] {}
+console.log(Math); //Object [Math] {} object
 
-console.log(Math.abs(-4));//4
+console.log(Math.abs(-4)); //4
 
 console.log(Math.round(4.6)); //5
 
-console.log(Math.ceil(4.2));//5 Note:-ceil means top value
+console.log(Math.ceil(4.2)); //5 Note:- ceil means top value
 
-console.log(Math.floor(4.9));//4
+console.log(Math.floor(4.9)); //4
 
-console.log(Math.min(4, 3, 6, 8));//3
+console.log(Math.min(4, 3, 6, 8)); //3
 
-console.log(Math.max(4, 3, 6, 8));//8
+console.log(Math.max(4, 3, 6, 8)); //8
 
-console.log(Math.random());//0.31599173088577537   (: Give value in Range 0 to 1
+console.log(Math.random()); //0.31599173088577537   (: Give value in Range 0 to 1
 
 console.log((Math.random()*10) + 1);
 
