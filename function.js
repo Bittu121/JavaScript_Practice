f0e3136d2ba3c23ee37cd4e7c43be65a1a1d3c3c
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-11-13 23:26:55 +0530

    Update function.js

--- function.js
+++ function.js
@@ -73,7 +73,5 @@ const myNewArray = [200, 400, 100, 600]
 function returnSecondValue(getArray){
     return getArray[1]
 }
-
 console.log(returnSecondValue(myNewArray)); //400
-
 console.log(returnSecondValue([200, 400, 500, 1000])); //400
